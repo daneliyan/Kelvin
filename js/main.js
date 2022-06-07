d4c3c1d3cca95d1f97d5e5b8722dd95cfb3dd079
daneliyan <105802998+daneliyan@users.noreply.github.com>
2022-06-07 04:48:25 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const nav = document.querySelector('#nav');
+const navBtn = document.querySelector('#nav-btn');
+const navBtnImg = document.querySelector('#nav-btn-img');
+
+navBtn.onclick = () => {
+    if (nav.classList.toggle('open')) {
+        navBtnImg.src = "./img/icons/nav-close.svg";
+    } else {
+        navBtnImg.src = "./img/icons/nav-open.svg";
+    }
+}
+
+AOS.init();
+
+//для отмены постоянной анимации
+//AOS.init({once:true});
+//для отмены на другом устройстве
+//disable: 'mobile'
